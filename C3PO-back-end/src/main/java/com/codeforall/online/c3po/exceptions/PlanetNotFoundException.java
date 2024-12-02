81606be51e26dbf7c9958e00982ac2e8225201e1
amaropmc <amaro.pmc.se@gmail.com>
2024-12-02 13:15:59 +0000

    Changes from .idea

--- C3PO-back-end/src/main/java/com/codeforall/online/c3po/exceptions/PlanetNotFoundException.java
+++ C3PO-back-end/src/main/java/com/codeforall/online/c3po/exceptions/PlanetNotFoundException.java
@@ -2,6 +2,9 @@ package com.codeforall.online.c3po.exceptions;
 
 import com.codeforall.online.c3po.errors.ErrorMessage;
 
+/**
+ * Throws an exception if a Planet was not found
+ */
 public class PlanetNotFoundException extends C3POException {
 
     public PlanetNotFoundException() {
--- C3PO-back-end/src/main/java/com/codeforall/online/c3po/persistence/dao/dao.java
+++
@@ -1,4 +0,0 @@
-package com.codeforall.online.c3po.persistence.dao;
-
-public interface dao {
-}
