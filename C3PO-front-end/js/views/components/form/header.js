64703d4f3897fd480bf6a6529d7e0b33c30514ec
Carina <carina.isabel.l@hotmail.com>
2024-12-03 13:20:09 +0000

    Implement planet page

--- C3PO-front-end/js/views/components/form/header.js
+++ C3PO-front-end/js/views/components/form/header.js
@@ -2,10 +2,7 @@ import { div } from "/js/views/components/commons/div.js";
 
 function formHeader() {
   const profileHeader = div(["profile-header"]);
-  const noAvatar = div(["no-avatar"]);
-
-  profileHeader.appendChild(noAvatar);
-
+  
   return profileHeader;
 }
 
