10770cab2572f102bc1092726b5b2a68dd00175e
Carina <carina.isabel.l@hotmail.com>
2024-12-07 11:33:39 +0000

    inicial page design change

--- C3PO-front-end/js/controllers/home-page.js
+++ C3PO-front-end/js/controllers/home-page.js
@@ -2,6 +2,7 @@ import { formPage } from "/js/views/components/form/form.js";
 import { homePageHeader } from "/js/views/components/form/home-page-header.js";
 import { div } from "/js/views/components/commons/div.js";
 
+
 window.addEventListener("DOMContentLoaded", function () {
   initializeFormPage();
 });
@@ -9,17 +10,35 @@ window.addEventListener("DOMContentLoaded", function () {
 function initializeFormPage() {
   const container = document.querySelector(".container");
 
+  
+
   // Picture div 
   const imageDiv = div(["image-container"]);
   const image = document.createElement("img");
-  image.src = "/assets/C-3PO.jpg";
+  image.src = "/assets/c3po/C-3PO.png";
   image.alt = "C-3PO robot";
+  const eyeLeft = div(["eye-container"]);
+  const eyeRight = div(["eye-container", "eye-right"]);
+
+
+  imageDiv.appendChild(eyeLeft);
+  imageDiv.appendChild(eyeRight);
   imageDiv.appendChild(image);
+
+
 
   // Header and Form container 
   const headerFormDiv = div(["header-form-container"]);
+  
+    //Title div
+    const titleDiv = div(["title-container"]);
+    const h1 = document.createElement("h1"); 
+    h1.classList.add("custom-font");
+    h1.textContent = "C - 3 P 0";
+    titleDiv.appendChild(h1); 
+  
 
-  // Header div
+   // Header div
   const headerDiv = div(["home-header-container"]);
   renderHomePageHeader(headerDiv);
 
@@ -43,12 +62,15 @@ function initializeFormPage() {
   formDiv.appendChild(form);
 
   // Append Header and Form to their container
+  headerFormDiv.appendChild(titleDiv);
   headerFormDiv.appendChild(headerDiv); 
   headerFormDiv.appendChild(formDiv);
 
   // Append both main sections to the container
   container.appendChild(headerFormDiv);
   container.appendChild(imageDiv);
+  
+
  
 
   // Form submit event listener
@@ -56,17 +78,11 @@ function initializeFormPage() {
     event.preventDefault();
 
     const userName = userNameInput.value.trim();
-    const password = passwordInput.value.trim();
 
   });
 }
 
 function renderHomePageHeader(container) {
-  if (typeof homePageHeader !== "function") {
-    console.error("homePageHeader is not a valid function");
-    return;
-  }
-
   const header = homePageHeader(addNewEvent);
   container.appendChild(header);
 }
--- C3PO-front-end/routes.js
+++
@@ -1,7 +0,0 @@
-import { routes } from "./routes.js";
-
-// Redirect to the home page
-window.location.href = routes.homePage.path;
-
-// Redirect to the planet page
-window.location.href = routes.planet.path;
